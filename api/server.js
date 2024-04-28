9b547bd62c729cf06a1afe9c58eb17a6e23d993e
Parfene Daniel <160225733+ParfeneDaniel@users.noreply.github.com>
2024-04-29 02:16:44 +0300

    create db connections

--- api/server.js
+++ api/server.js
@@ -1,11 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import redis from "redis";
+import { connectToMongoDB } from "./db/config.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
 
 const app = express();
 
+const client = redis.createClient();
+
+client.connect();
+
+client.on("connect", () => {
+  console.log("Connected to RedisDB!");
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.listen(PORT, () => {
-    console.log("Server is running!");
-})
+  connectToMongoDB();
+  console.log("Server is running!"); 
+});
