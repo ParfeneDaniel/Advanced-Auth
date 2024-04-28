a6495a1e7eaa04e7217890ca52eb2a3cd47ad309
Parfene Daniel <160225733+ParfeneDaniel@users.noreply.github.com>
2024-04-29 02:28:58 +0300

    create auth router

---
+++ api/controllers/auth.controllers.js
@@ -0,0 +1,11 @@
+export const signUp = (req, res) => {
+  res.status(201).json({ message: "Success" });
+};
+
+export const signIn = (req, res) => {
+  res.status(201).json({ message: "Success" });
+};
+
+export const signOut = (req, res) => {
+  res.status(201).json({ message: "Success" });
+};
--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import redis from "redis";
 import { connectToMongoDB } from "./db/config.js";
+import authRouter from "./routes/auth.router.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -18,6 +19,8 @@ client.on("connect", () => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
   connectToMongoDB();
