6878a02cbc52f93870aceaeb3fba1d84dbe24e12
Parfene Daniel <160225733+ParfeneDaniel@users.noreply.github.com>
2024-04-29 14:50:39 +0300

    config check middleware

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,5 +1,17 @@
+import { validationResult } from "express-validator";
+
 export const signUp = (req, res) => {
-  res.status(201).json({ message: "Success" });
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    res.status(201).json({ message: "All data is good" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", errors: error.message });
+  }
 };
 
 export const signIn = (req, res) => {
@@ -7,5 +19,5 @@ export const signIn = (req, res) => {
 };
 
 export const signOut = (req, res) => {
-  res.status(201).json({ message: "Success" });
+  res.status(201).json({ message: "Success" }); 
 };
--- api/routes/auth.router.js
+++ api/routes/auth.router.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { signIn, signOut, signUp } from "../controllers/auth.controllers.js";
+import { userValidator } from "../models/checkUser.model.js";
 
 const router = express.Router();
 
-router.post("/signup", signUp);
+router.post("/signup", userValidator, signUp);
 router.post("/signin", signIn);
 router.post("/signout", signOut);
 
